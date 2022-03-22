c5139f74d33d08d0ed40d62394234cfe28e91611
Pranav Bahuguna <64610291+pranav2274@users.noreply.github.com>
2022-03-22 10:36:17 +0530

    Update mtsqlconnection.java

--- JDBC/mtsqlconnection.java
+++ JDBC/mtsqlconnection.java
@@ -1,9 +1,9 @@
-package backend;
+//importing the required classes from java.sql package
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class Connectionmanager {
-	private static String url = "jdbc:mysql://localhost:3306/grey_goose";    
+    private static String url = "jdbc:mysql://localhost:3306/grey_goose";    
     private static String driverName = "com.mysql.cj.jdbc.Driver";   
     private static String username = "root";   
     private static String password = "root@123";
@@ -11,14 +11,14 @@ public class Connectionmanager {
     private static String urlstring;
 
     public static Connection getConnect() throws ClassNotFoundException {
-      Class.forName(driverName);
+      Class.forName(driverName);                                           // registerd the JDBC driver
             try {
-               con = DriverManager.getConnection(url, username, password);
+               con = DriverManager.getConnection(url, username, password); // formulate the JDBC url and establishing the connection
             } catch (SQLException ex) {
                 // log an exception. fro example:
                 System.out.println("Failed to create the database connection."); 
             }
-        return con;
+        return con;                                                         // returning the connection object
     }
 }
 
